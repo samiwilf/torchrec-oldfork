22599b072368f2b1272ae853c8aa60671984c685
Sami Wilf <samiw1@fb.com>
2022-03-24 10:26:23 +0000

    update

--- examples/dlrm/dlrm_main.py
+++ examples/dlrm/dlrm_main.py
@@ -68,17 +68,71 @@ from torchrec.distributed.types import ModuleSharder, ShardingType
 import torch.nn as nn
 
 class TestEBCSharder(EmbeddingBagCollectionSharder):
-        def __init__(
-            self,
-            sharding_type: str,
-            kernel_type: str,
-            fused_params: Optional[Dict[str, Any]] = None,
-        ) -> None:
-            if fused_params is None:
-                fused_params = {}
-            self._sharding_type = sharding_type
-            self._kernel_type = kernel_type
-            self._fused_params = fused_params
+    def __init__(
+        self,
+        sharding_type: str,
+        kernel_type: str,
+        fused_params: Optional[Dict[str, Any]] = None,
+    ) -> None:
+        if fused_params is None:
+            fused_params = {}
+        # self._sharding_type = sharding_type
+        # self._kernel_type = kernel_type
+        self._fused_params = fused_params
+        self._sharding_type = [
+            ShardingType.DATA_PARALLEL.value,
+            ShardingType.TABLE_WISE.value,
+            ShardingType.COLUMN_WISE.value,
+            ShardingType.ROW_WISE.value,
+            ShardingType.TABLE_ROW_WISE.value,
+            ShardingType.TABLE_COLUMN_WISE.value,
+            ]  
+        self._kernel_type = [
+            EmbeddingComputeKernel.DENSE.value, 
+            EmbeddingComputeKernel.SPARSE.value,
+            # EmbeddingComputeKernel.BATCHED_DENSE.value,
+            # EmbeddingComputeKernel.BATCHED_FUSED.value,
+            # EmbeddingComputeKernel.BATCHED_FUSED_UVM.value,
+            # EmbeddingComputeKernel.BATCHED_FUSED_UVM_CACHING.value,
+            # EmbeddingComputeKernel.BATCHED_QUANT.value,    
+        ]
+    """
+    Restricts sharding to single type only.
+    """
+
+    def sharding_types(self, compute_device_type: str) -> List[str]:
+        #return [self._sharding_type]
+        return [
+            ShardingType.DATA_PARALLEL.value,
+            ShardingType.TABLE_WISE.value,
+            ShardingType.COLUMN_WISE.value,
+            ShardingType.ROW_WISE.value,
+            ShardingType.TABLE_ROW_WISE.value,
+            ShardingType.TABLE_COLUMN_WISE.value,
+            ]        
+
+    """
+    Restricts to single impl.
+    """
+
+    def compute_kernels(
+        self, sharding_type: str, compute_device_type: str
+    ) -> List[str]:
+        #return [self._kernel_type]
+        return [
+            EmbeddingComputeKernel.DENSE.value, 
+            EmbeddingComputeKernel.SPARSE.value,
+            # EmbeddingComputeKernel.BATCHED_DENSE.value,
+            # EmbeddingComputeKernel.BATCHED_FUSED.value,
+            # EmbeddingComputeKernel.BATCHED_FUSED_UVM.value,
+            # EmbeddingComputeKernel.BATCHED_FUSED_UVM_CACHING.value,
+            # EmbeddingComputeKernel.BATCHED_QUANT.value,    
+        ]
+
+    @property
+    def fused_params(self) -> Optional[Dict[str, Any]]:
+        return self._fused_params
+
 
 class TWSharder(EmbeddingBagCollectionSharder):
     def sharding_types(self, compute_device_type: str) -> List[str]:
@@ -461,7 +515,7 @@ def main(argv: List[str]) -> None:
     # ]
 
     if True:
-        if False:
+        if True:
             sharders = TestEBCSharder(
                             sharding_type=ShardingType.TABLE_WISE.value,
                             kernel_type=EmbeddingComputeKernel.SPARSE.value,
